68adb11afcfdbe1f3675c3eeaac7c179d7bb1c98
sean <sean870603tw@yahoo.com.tw>
2020-10-31 16:58:36 +0800

    init repo

---
+++ SeanStudy/ConvexHull/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvexHull
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    var inputImage = new Image<Gray, byte>(ofd.FileName);
+                    inputImage = inputImage.ThresholdBinary(new Gray(100), new Gray(255));
+
+                    doConvexHull(inputImage);
+                }
+            }
+        }
+
+        private static void doConvexHull(Image<Gray, byte> image)
+        {
+            var imgBGR = image.Convert<Bgra, byte>();
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(image, contours, null, RetrType.External, ChainApproxMethod.ChainApproxNone);
+
+                PointF[] maxContourPtFs = new PointF[1];
+                int maxSize = 0;
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var contour = contours[i];
+                    PointF[] temp = Array.ConvertAll(contour.ToArray(), new Converter<Point, PointF>(Point2PointF));
+                    PointF[] pts = CvInvoke.ConvexHull(temp, true);
+                    if(pts.Length>maxSize)
+                    {
+                        maxSize = pts.Length;
+                        maxContourPtFs = pts;
+                    }
+                }
+
+                Point[] maxContourPts = new Point[maxContourPtFs.Length];
+                for (int i = 0; i < maxContourPtFs.Length; i++)
+                {
+                    maxContourPts[i] = new Point((int)maxContourPtFs[i].X, (int)maxContourPtFs[i].Y);
+                }
+
+                Point[] newMaxContourPts = new Point[23];
+
+                newMaxContourPts[0] = maxContourPts[0];
+                newMaxContourPts[1] = maxContourPts[1];
+                newMaxContourPts[2] = new Point(70, 190);
+                newMaxContourPts[3] = maxContourPts[2];
+                newMaxContourPts[4] = new Point(90, 228);
+                newMaxContourPts[5] = maxContourPts[3];
+                newMaxContourPts[6] = new Point(120, 245);
+                newMaxContourPts[7] = maxContourPts[4];
+                newMaxContourPts[8] = new Point(161, 252);
+                newMaxContourPts[9] = maxContourPts[5];
+                newMaxContourPts[10] = new Point(196, 225);
+                newMaxContourPts[11] = new Point(225, 226);
+                newMaxContourPts[12] = new Point(214, 193);
+                newMaxContourPts[13] = maxContourPts[7];
+                newMaxContourPts[14] = new Point(188, 143);
+                newMaxContourPts[15] = maxContourPts[8];
+                newMaxContourPts[16] = new Point(188, 114);
+                newMaxContourPts[17] = maxContourPts[9];
+                newMaxContourPts[18] = new Point(162, 99);
+                newMaxContourPts[19] = maxContourPts[11];
+                newMaxContourPts[20] = new Point(130, 108);
+                newMaxContourPts[21] = maxContourPts[13];
+                newMaxContourPts[22] = new Point(107, 137);
+
+                imgBGR.Draw(maxContourPts, new Bgra(0, 0, 255, 255), 2);
+                imgBGR.Draw(newMaxContourPts, new Bgra(0, 255, 0, 255), 2);
+
+                //for (int j = 0; j < maxContourPtFs.Length; j++)
+                //{
+                //    Point p1 = new Point((int)maxContourPtFs[j].X, (int)maxContourPtFs[j].Y);
+                //    Point p2;
+
+                //    if (j == maxContourPtFs.Length - 1)
+                //        p2 = new Point((int)maxContourPtFs[0].X, (int)maxContourPtFs[0].Y);
+                //    else
+                //        p2 = new Point((int)maxContourPtFs[j + 1].X, (int)maxContourPtFs[j + 1].Y);
+
+                //    CvInvoke.Line(imgBGR, p1, p2, new MCvScalar(255, 0, 255, 255), 3);
+                //}
+
+            }
+            
+
+            CvInvoke.Imshow("thres100", imgBGR);
+            //imgBGR.Save("thres100.png");
+
+        }
+
+
+
+
+        private static PointF Point2PointF(Point P)
+        {
+            PointF PF = new PointF
+            {
+                X = P.X,
+                Y = P.Y
+            };
+            return PF;
+        }
+    }
+}
---
+++ SeanStudy/SeanStudy/Form1.cs
@@ -0,0 +1,138 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Corex.VisionLib.Algorithm;
+using Corex.VisionLib;
+using System.Diagnostics;
+using Emgu.CV.CvEnum;
+
+namespace SeanStudy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Stopwatch sw = new Stopwatch();
+                    var inputImage = new Image<Gray, byte>(ofd.FileName);
+                    inputImage = inputImage.ThresholdBinary(new Gray(127), new Gray(255));
+
+                    MeasureAngle.Parameter parameter = new MeasureAngle.Parameter();
+                    parameter.ROI1 = new ROIOption(ROIOption.ROIType.SpecROI);
+                    parameter.ROI1.SpecROI = SpecROI.NewLeftTopModeROI(55, 25, 200, 120);
+                    parameter.ROI2 = new ROIOption(ROIOption.ROIType.SpecROI);
+                    parameter.ROI2.SpecROI = SpecROI.NewLeftTopModeROI(255, 140, 135, 220);
+                    parameter.AnglePoint = new Point(315, 75);
+
+                    MeasureAngle.Result result = new MeasureAngle.Result(0);
+                    sw.Start();
+                    FindRowCol(inputImage, parameter, out result);
+                    sw.Stop();
+
+                    TimeSpan time = sw.Elapsed;
+                    Console.WriteLine(time.TotalMilliseconds.ToString() + "ms");
+                }
+            }
+        }
+
+        public static bool FindRowCol(Image<Gray, byte> image, MeasureAngle.Parameter parameter, out MeasureAngle.Result result)
+        {
+            var img1 = ROIOption.GetImage(image, parameter.ROI1);
+            var img2 = ROIOption.GetImage(image, parameter.ROI2);
+
+            int row = horizontalProjection(img1);
+            int col = verticalProjection(img2);
+
+            List<Point> rowLinePts = new List<Point>();
+            Point firstPt = new Point(0, row);
+            Point lastPt = new Point(parameter.ROI1.SpecROI.Width - 1, row);
+            firstPt = ROIOption.GetOriginPoint(image.Size, firstPt, parameter.ROI1);
+            lastPt = ROIOption.GetOriginPoint(image.Size, lastPt, parameter.ROI1);
+            rowLinePts.Add(firstPt);
+            rowLinePts.Add(lastPt);
+
+
+            List<Point> colLinePts = new List<Point>();
+            firstPt = new Point(col, 0);
+            lastPt = new Point(col, parameter.ROI2.SpecROI.Height - 1);
+            firstPt = ROIOption.GetOriginPoint(image.Size, firstPt, parameter.ROI2);
+            lastPt = ROIOption.GetOriginPoint(image.Size, lastPt, parameter.ROI2);
+            colLinePts.Add(firstPt);
+            colLinePts.Add(lastPt);
+
+            result = new MeasureAngle.Result(0);
+            if (MeasureAngle.Find(rowLinePts, colLinePts, parameter.AnglePoint, out result))
+            {
+                #region [DEBUG] Draw
+                var imgBGRA = image.Convert<Bgra, byte>();
+                // draw horizontal line
+                LineSegment2D rowLine = new LineSegment2D(result.ROI1Points[0], result.ROI1Points[result.ROI1Points.Count - 1]);
+                imgBGRA.Draw(rowLine, new Bgra(0, 0, 255, 255), 1);
+
+                // draw vertical line
+                LineSegment2D colLine = new LineSegment2D(result.ROI2Points[0], result.ROI2Points[result.ROI2Points.Count - 1]);
+                imgBGRA.Draw(colLine, new Bgra(0, 255, 0, 255), 1);
+
+                CvInvoke.PutText(imgBGRA, result.Angle.ToString(), parameter.AnglePoint, FontFace.HersheyComplex, 1, new MCvScalar(255, 0, 0, 255));
+                CvInvoke.Imshow("imgBGRA", imgBGRA);
+                #endregion
+
+                return true;
+            }
+            return false;
+        }
+
+        private static int verticalProjection(Image<Gray, byte> image)
+        {
+            // vertical projection
+            int width = image.Width;
+            int height = image.Height;
+            int maxCol = 0;
+
+            // reduce to a single column by sum
+            Mat mat = new Mat();
+            CvInvoke.Reduce(image, mat, ReduceDimension.SingleRow, ReduceType.ReduceSum, DepthType.Cv32S);
+            int[] projSums = new int[width];
+            System.Runtime.InteropServices.Marshal.Copy(mat.DataPointer, projSums, 0, width);
+            int minVal = projSums.Min();
+            maxCol = projSums.ToList().IndexOf(minVal);
+
+            return maxCol;
+        }
+
+        private static int horizontalProjection(Image<Gray, byte> image)
+        {
+            // horizontal projection
+            int width = image.Width;
+            int height = image.Height;
+            int maxRow = 0;
+
+            // reduce to a single column by sum
+            Mat mat = new Mat();
+            CvInvoke.Reduce(image, mat, ReduceDimension.SingleCol, ReduceType.ReduceSum, DepthType.Cv32S);
+            int[] projSums = new int[height];
+            System.Runtime.InteropServices.Marshal.Copy(mat.DataPointer, projSums, 0, height);
+            int minVal = projSums.Min();
+            maxRow = projSums.ToList().IndexOf(minVal);
+
+            return maxRow;
+        }
+
+    }
+}
