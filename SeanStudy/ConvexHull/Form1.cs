8c4cddf4fdc31f83b209daab5160ce9ddce26a8d
SeanLin <sean870603tw@yahoo.com.tw>
2020-11-01 01:11:11 +0800

    Update convexityDefect

--- SeanStudy/ConvexHull/Form1.cs
+++ SeanStudy/ConvexHull/Form1.cs
@@ -30,6 +30,7 @@ namespace ConvexHull
                     var inputImage = new Image<Gray, byte>(ofd.FileName);
                     inputImage = inputImage.ThresholdBinary(new Gray(100), new Gray(255));
 
+                    //CvInvoke.Imshow("inputImage", inputImage);
                     doConvexHull(inputImage);
                 }
             }
@@ -39,92 +40,45 @@ namespace ConvexHull
         {
             var imgBGR = image.Convert<Bgra, byte>();
 
+            using (Mat defect = new Mat())
+            using (VectorOfInt hull = new VectorOfInt())
             using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
             {
                 CvInvoke.FindContours(image, contours, null, RetrType.External, ChainApproxMethod.ChainApproxNone);
+                //CvInvoke.DrawContours(imgBGR, contours, -1, new MCvScalar(0, 255, 0, 255), 2);
 
-                PointF[] maxContourPtFs = new PointF[1];
-                int maxSize = 0;
 
                 for (int i = 0; i < contours.Size; i++)
                 {
-                    var contour = contours[i];
-                    PointF[] temp = Array.ConvertAll(contour.ToArray(), new Converter<Point, PointF>(Point2PointF));
-                    PointF[] pts = CvInvoke.ConvexHull(temp, true);
-                    if(pts.Length>maxSize)
+                    VectorOfPoint contour = contours[i];
+                    CvInvoke.ConvexHull(contour, hull);
+                    CvInvoke.ConvexityDefects(contour, hull, defect);
+
+                    //convexity defect is a four channel mat, when k rows and 1 cols, where k = the number of convexity defects. 
+                    if (!defect.IsEmpty)
                     {
-                        maxSize = pts.Length;
-                        maxContourPtFs = pts;
+                        //Data from Mat are not directly readable so we convert it to Matrix<>
+                        Matrix<int> defectMatrix = new Matrix<int>(defect.Rows, defect.Cols, defect.NumberOfChannels);
+                        defect.CopyTo(defectMatrix);
+
+                        for (int j = 0; j < defectMatrix.Rows; j++)
+                        {
+                            int startIdx = defectMatrix.Data[j, 0];
+                            int endIdx = defectMatrix.Data[j, 1];
+                            int farIdx = defectMatrix.Data[j, 2];
+                            Point startPoint = contour[startIdx];
+                            Point endPoint = contour[endIdx];
+                            Point farPoint = contour[farIdx];
+                            
+                            // draw a line connecting the convexity defect start point and end point in thin red line
+                            CvInvoke.Line(imgBGR, startPoint, endPoint, new MCvScalar(0, 0, 255, 255), 2);
+                            CvInvoke.Circle(imgBGR, farPoint, 1, new MCvScalar(0, 255, 0, 255), 2);
+                        }
                     }
                 }
-
-                Point[] maxContourPts = new Point[maxContourPtFs.Length];
-                for (int i = 0; i < maxContourPtFs.Length; i++)
-                {
-                    maxContourPts[i] = new Point((int)maxContourPtFs[i].X, (int)maxContourPtFs[i].Y);
-                }
-
-                Point[] newMaxContourPts = new Point[23];
-
-                newMaxContourPts[0] = maxContourPts[0];
-                newMaxContourPts[1] = maxContourPts[1];
-                newMaxContourPts[2] = new Point(70, 190);
-                newMaxContourPts[3] = maxContourPts[2];
-                newMaxContourPts[4] = new Point(90, 228);
-                newMaxContourPts[5] = maxContourPts[3];
-                newMaxContourPts[6] = new Point(120, 245);
-                newMaxContourPts[7] = maxContourPts[4];
-                newMaxContourPts[8] = new Point(161, 252);
-                newMaxContourPts[9] = maxContourPts[5];
-                newMaxContourPts[10] = new Point(196, 225);
-                newMaxContourPts[11] = new Point(225, 226);
-                newMaxContourPts[12] = new Point(214, 193);
-                newMaxContourPts[13] = maxContourPts[7];
-                newMaxContourPts[14] = new Point(188, 143);
-                newMaxContourPts[15] = maxContourPts[8];
-                newMaxContourPts[16] = new Point(188, 114);
-                newMaxContourPts[17] = maxContourPts[9];
-                newMaxContourPts[18] = new Point(162, 99);
-                newMaxContourPts[19] = maxContourPts[11];
-                newMaxContourPts[20] = new Point(130, 108);
-                newMaxContourPts[21] = maxContourPts[13];
-                newMaxContourPts[22] = new Point(107, 137);
-
-                imgBGR.Draw(maxContourPts, new Bgra(0, 0, 255, 255), 2);
-                imgBGR.Draw(newMaxContourPts, new Bgra(0, 255, 0, 255), 2);
-
-                //for (int j = 0; j < maxContourPtFs.Length; j++)
-                //{
-                //    Point p1 = new Point((int)maxContourPtFs[j].X, (int)maxContourPtFs[j].Y);
-                //    Point p2;
-
-                //    if (j == maxContourPtFs.Length - 1)
-                //        p2 = new Point((int)maxContourPtFs[0].X, (int)maxContourPtFs[0].Y);
-                //    else
-                //        p2 = new Point((int)maxContourPtFs[j + 1].X, (int)maxContourPtFs[j + 1].Y);
-
-                //    CvInvoke.Line(imgBGR, p1, p2, new MCvScalar(255, 0, 255, 255), 3);
-                //}
-
             }
-            
 
             CvInvoke.Imshow("thres100", imgBGR);
-            //imgBGR.Save("thres100.png");
-
-        }
-
-
-
-
-        private static PointF Point2PointF(Point P)
-        {
-            PointF PF = new PointF
-            {
-                X = P.X,
-                Y = P.Y
-            };
-            return PF;
         }
     }
 }
