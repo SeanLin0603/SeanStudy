c6e872a6fa0e3ef6323918c062975b3801e319d1
sean <sean870603tw@yahoo.com.tw>
2021-02-22 12:46:03 +0800

    Compute EMD  by feeding 1D array

---
+++ SeanStudy/EMD/EMDForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace EMD
+{
+    partial class EMDForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadPic1 = new System.Windows.Forms.Button();
+            this.btnLoadPic2 = new System.Windows.Forms.Button();
+            this.picImg1 = new System.Windows.Forms.PictureBox();
+            this.picImg2 = new System.Windows.Forms.PictureBox();
+            this.lblEMD = new System.Windows.Forms.Label();
+            this.lblCorrel = new System.Windows.Forms.Label();
+            this.lblChisqr = new System.Windows.Forms.Label();
+            this.lblIntersect = new System.Windows.Forms.Label();
+            this.lblBhattacharyya = new System.Windows.Forms.Label();
+            this.lblChisqrAlt = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.picHist1 = new System.Windows.Forms.PictureBox();
+            this.picHist2 = new System.Windows.Forms.PictureBox();
+            this.txtBinSize = new System.Windows.Forms.TextBox();
+            this.lblBinSize = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHist1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHist2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadPic1
+            // 
+            this.btnLoadPic1.Location = new System.Drawing.Point(11, 11);
+            this.btnLoadPic1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoadPic1.Name = "btnLoadPic1";
+            this.btnLoadPic1.Size = new System.Drawing.Size(65, 35);
+            this.btnLoadPic1.TabIndex = 0;
+            this.btnLoadPic1.Text = "Load1";
+            this.btnLoadPic1.UseVisualStyleBackColor = true;
+            this.btnLoadPic1.Click += new System.EventHandler(this.btnLoadPic1_Click);
+            // 
+            // btnLoadPic2
+            // 
+            this.btnLoadPic2.Location = new System.Drawing.Point(509, 9);
+            this.btnLoadPic2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoadPic2.Name = "btnLoadPic2";
+            this.btnLoadPic2.Size = new System.Drawing.Size(65, 35);
+            this.btnLoadPic2.TabIndex = 0;
+            this.btnLoadPic2.Text = "Load2";
+            this.btnLoadPic2.UseVisualStyleBackColor = true;
+            this.btnLoadPic2.Click += new System.EventHandler(this.btnLoadPic2_Click);
+            // 
+            // picImg1
+            // 
+            this.picImg1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picImg1.Location = new System.Drawing.Point(11, 51);
+            this.picImg1.Name = "picImg1";
+            this.picImg1.Size = new System.Drawing.Size(226, 244);
+            this.picImg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImg1.TabIndex = 1;
+            this.picImg1.TabStop = false;
+            // 
+            // picImg2
+            // 
+            this.picImg2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picImg2.Location = new System.Drawing.Point(509, 51);
+            this.picImg2.Name = "picImg2";
+            this.picImg2.Size = new System.Drawing.Size(226, 244);
+            this.picImg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImg2.TabIndex = 1;
+            this.picImg2.TabStop = false;
+            // 
+            // lblEMD
+            // 
+            this.lblEMD.AutoSize = true;
+            this.lblEMD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEMD.Location = new System.Drawing.Point(10, 470);
+            this.lblEMD.Name = "lblEMD";
+            this.lblEMD.Size = new System.Drawing.Size(44, 16);
+            this.lblEMD.TabIndex = 2;
+            this.lblEMD.Text = "EMD: ";
+            // 
+            // lblCorrel
+            // 
+            this.lblCorrel.AutoSize = true;
+            this.lblCorrel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorrel.Location = new System.Drawing.Point(10, 320);
+            this.lblCorrel.Name = "lblCorrel";
+            this.lblCorrel.Size = new System.Drawing.Size(79, 16);
+            this.lblCorrel.TabIndex = 2;
+            this.lblCorrel.Text = "Correlation: ";
+            // 
+            // lblChisqr
+            // 
+            this.lblChisqr.AutoSize = true;
+            this.lblChisqr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChisqr.Location = new System.Drawing.Point(10, 350);
+            this.lblChisqr.Name = "lblChisqr";
+            this.lblChisqr.Size = new System.Drawing.Size(81, 16);
+            this.lblChisqr.TabIndex = 3;
+            this.lblChisqr.Text = "Chi-Square: ";
+            // 
+            // lblIntersect
+            // 
+            this.lblIntersect.AutoSize = true;
+            this.lblIntersect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntersect.Location = new System.Drawing.Point(10, 380);
+            this.lblIntersect.Name = "lblIntersect";
+            this.lblIntersect.Size = new System.Drawing.Size(82, 16);
+            this.lblIntersect.TabIndex = 3;
+            this.lblIntersect.Text = "Intersection: ";
+            // 
+            // lblBhattacharyya
+            // 
+            this.lblBhattacharyya.AutoSize = true;
+            this.lblBhattacharyya.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBhattacharyya.Location = new System.Drawing.Point(10, 410);
+            this.lblBhattacharyya.Name = "lblBhattacharyya";
+            this.lblBhattacharyya.Size = new System.Drawing.Size(100, 16);
+            this.lblBhattacharyya.TabIndex = 3;
+            this.lblBhattacharyya.Text = "Bhattacharyya: ";
+            // 
+            // lblChisqrAlt
+            // 
+            this.lblChisqrAlt.AutoSize = true;
+            this.lblChisqrAlt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChisqrAlt.Location = new System.Drawing.Point(10, 440);
+            this.lblChisqrAlt.Name = "lblChisqrAlt";
+            this.lblChisqrAlt.Size = new System.Drawing.Size(147, 16);
+            this.lblChisqrAlt.TabIndex = 3;
+            this.lblChisqrAlt.Text = "Alternative Chi-Square: ";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(341, 51);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(65, 35);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calc";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // picHist1
+            // 
+            this.picHist1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picHist1.Location = new System.Drawing.Point(251, 173);
+            this.picHist1.Name = "picHist1";
+            this.picHist1.Size = new System.Drawing.Size(114, 123);
+            this.picHist1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHist1.TabIndex = 1;
+            this.picHist1.TabStop = false;
+            // 
+            // picHist2
+            // 
+            this.picHist2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picHist2.Location = new System.Drawing.Point(378, 173);
+            this.picHist2.Name = "picHist2";
+            this.picHist2.Size = new System.Drawing.Size(114, 123);
+            this.picHist2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHist2.TabIndex = 1;
+            this.picHist2.TabStop = false;
+            // 
+            // txtBinSize
+            // 
+            this.txtBinSize.Location = new System.Drawing.Point(378, 118);
+            this.txtBinSize.Name = "txtBinSize";
+            this.txtBinSize.Size = new System.Drawing.Size(100, 20);
+            this.txtBinSize.TabIndex = 4;
+            this.txtBinSize.Text = "256";
+            this.txtBinSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBinSize
+            // 
+            this.lblBinSize.AutoSize = true;
+            this.lblBinSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinSize.Location = new System.Drawing.Point(294, 119);
+            this.lblBinSize.Name = "lblBinSize";
+            this.lblBinSize.Size = new System.Drawing.Size(62, 16);
+            this.lblBinSize.TabIndex = 5;
+            this.lblBinSize.Text = "BinSize : ";
+            // 
+            // EMDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 518);
+            this.Controls.Add(this.lblBinSize);
+            this.Controls.Add(this.txtBinSize);
+            this.Controls.Add(this.lblChisqrAlt);
+            this.Controls.Add(this.lblBhattacharyya);
+            this.Controls.Add(this.lblIntersect);
+            this.Controls.Add(this.lblChisqr);
+            this.Controls.Add(this.lblCorrel);
+            this.Controls.Add(this.lblEMD);
+            this.Controls.Add(this.picImg2);
+            this.Controls.Add(this.picHist2);
+            this.Controls.Add(this.picHist1);
+            this.Controls.Add(this.picImg1);
+            this.Controls.Add(this.btnLoadPic2);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnLoadPic1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "EMDForm";
+            this.Text = "Earth Mover\'s Distance";
+            ((System.ComponentModel.ISupportInitialize)(this.picImg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHist1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHist2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadPic1;
+        private System.Windows.Forms.Button btnLoadPic2;
+        private System.Windows.Forms.PictureBox picImg1;
+        private System.Windows.Forms.PictureBox picImg2;
+        private System.Windows.Forms.Label lblEMD;
+        private System.Windows.Forms.Label lblCorrel;
+        private System.Windows.Forms.Label lblChisqr;
+        private System.Windows.Forms.Label lblIntersect;
+        private System.Windows.Forms.Label lblBhattacharyya;
+        private System.Windows.Forms.Label lblChisqrAlt;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.PictureBox picHist1;
+        private System.Windows.Forms.PictureBox picHist2;
+        private System.Windows.Forms.TextBox txtBinSize;
+        private System.Windows.Forms.Label lblBinSize;
+    }
+}
+
---
+++ SeanStudy/EMD/EMDForm.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using System.Runtime.InteropServices;
+
+namespace EMD
+{
+    public partial class EMDForm : Form
+    {
+        public static float[] HistValF1;
+        public static float[] HistValF2;
+        public static Image<Gray, float> HistImg1;
+        public static Image<Gray, float> HistImg2;
+        public static int BinSize;
+
+        public EMDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadPic1_Click(object sender, EventArgs e)
+        {
+            BinSize = int.Parse(txtBinSize.Text);
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Image<Gray, byte> image1 = new Image<Gray, byte>(ofd.FileName);
+                    picImg1.Image = image1.Bitmap;
+
+                    HistValF1 = getHistogram(image1, BinSize);
+                    Image<Gray, byte> histImg = drawHistogram(HistValF1, BinSize);
+                    HistImg1 = histImg.Convert<Gray, float>();
+                    picHist1.Image = histImg.Bitmap;
+                }
+            }
+        }
+
+        private void btnLoadPic2_Click(object sender, EventArgs e)
+        {
+            BinSize = int.Parse(txtBinSize.Text);
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Image<Gray, byte> image2 = new Image<Gray, byte>(ofd.FileName);
+                    picImg2.Image = image2.Bitmap;
+
+                    HistValF2 = getHistogram(image2, BinSize);
+                    Image<Gray, byte> histImg = drawHistogram(HistValF2, BinSize);
+                    HistImg2 = histImg.Convert<Gray, float>();
+                    picHist2.Image = histImg.Bitmap;
+                }
+            }
+        }
+
+        private static float[] getHistogram(Image<Gray, byte> image, int binSize)
+        {
+            RangeF range = new RangeF(0, 256);
+            DenseHistogram hist = new DenseHistogram(binSize, range);
+            Image<Gray, byte>[] images = new Image<Gray, byte>[] { image };
+            hist.Calculate(images, false, null);
+
+            float[] histValF = new float[hist.Rows];
+            Marshal.Copy(hist.DataPointer, histValF, 0, hist.Rows);
+
+            return histValF;
+        }
+
+        private static Image<Gray, byte> drawHistogram(float[] histArray, int binSize)
+        {
+            float maxVal = histArray.Max();
+
+            Image<Gray, byte> histImg = new Image<Gray, byte>(binSize, binSize, new Gray(255));
+            for (int i = 0; i < binSize; i++)
+            {
+                // normalization
+                int y = binSize - (int)(histArray[i] / maxVal * binSize) - 1;
+                Point p1 = new Point(i, y);
+                Point p2 = new Point(i, binSize - 1);
+                LineSegment2D line = new LineSegment2D(p1, p2);
+                histImg.Draw(line, new Gray(0), 1);
+            }
+            return histImg;
+        }
+
+        //private static Mat calcHistHSV(Image<Gray, byte> image)
+        //{
+        //    int[] channels = new int[] { 0, 1};
+        //    int[] histbins = new int[] { 256, 256 };
+        //    float[] ranges = new float[] { 0.0f, 360.0f, 0.0f, 256.0f };
+
+        //    Mat hist = new Mat();
+        //    VectorOfMat vecOfMat = new VectorOfMat();
+        //    Image<Hsv, float> imghsv = image.Convert<Hsv, float>();
+        //    vecOfMat.Push(imghsv);
+        //    CvInvoke.CalcHist(vecOfMat, channels, null, hist, histbins, ranges, false);
+
+        //    return hist;
+        //}
+
+        //private static double earthMoverDistance(Mat mat1, Mat mat2, int histWidth, int histHeight)
+        //{
+        //    //CvInvoke.Normalize(hist1, hist1, 0, 1, NormType.MinMax);
+        //    //CvInvoke.Normalize(hist2, hist2, 0, 1, NormType.MinMax);
+
+        //    Matrix<float> signature1 = new Matrix<float>(histWidth * histHeight, 3, 1);
+        //    Matrix<float> signature2 = new Matrix<float>(histWidth * histHeight, 3, 1);
+        //    Matrix<float> matrixHist1 = new Matrix<float>(histHeight, histWidth);
+        //    Matrix<float> matrixHist2 = new Matrix<float>(histHeight, histWidth);
+        //    mat1.CopyTo(matrixHist1);
+        //    mat2.CopyTo(matrixHist2);
+
+        //    for (int r = 0; r < histHeight; r++)
+        //    {
+        //        for (int c = 0; c < histWidth; c++)
+        //        {
+        //            //matHist1;
+        //            signature1.Data[r * histWidth + c, 0] = matrixHist1.Data[r, c];
+        //            signature1.Data[r * histWidth + c, 1] = r;
+        //            signature1.Data[r * histWidth + c, 2] = c;
+
+        //            //matHist2;
+        //            signature2.Data[r * histWidth + c, 0] = matrixHist2.Data[r, c];
+        //            signature2.Data[r * histWidth + c, 1] = r;
+        //            signature2.Data[r * histWidth + c, 2] = c;
+        //        }
+        //    }
+
+        //    double emd = CvInvoke.EMD(signature1, signature2, DistType.L2);
+        //    return emd;
+        //}
+
+        private static double earthMoverDistance(float[] hist1, float[] hist2, int binSize)
+        {
+            Matrix<float> signature1 = new Matrix<float>(binSize, 2, 1);
+            Matrix<float> signature2 = new Matrix<float>(binSize, 2, 1);
+
+            for (int i = 0; i < binSize; i++)
+            {
+                signature1.Data[i, 0] = hist1[i];
+                signature1.Data[i, 1] = i;
+
+                signature2.Data[i, 0] = hist2[i];
+                signature2.Data[i, 1] = i;
+            }
+
+            //for (int r = 0; r < histHeight; r++)
+            //{
+            //    for (int c = 0; c < histWidth; c++)
+            //    {
+            //        //matHist1;
+            //        signature1.Data[r * histWidth + c, 0] = hist1.Data[r, c, 0] / 255;
+            //        signature1.Data[r * histWidth + c, 1] = r;
+            //        signature1.Data[r * histWidth + c, 2] = c;
+
+            //        //matHist2;
+            //        signature2.Data[r * histWidth + c, 0] = hist2.Data[r, c, 0] / 255;
+            //        signature2.Data[r * histWidth + c, 1] = r;
+            //        signature2.Data[r * histWidth + c, 2] = c;
+            //    }
+            //}
+
+            double emd = CvInvoke.EMD(signature1, signature2, DistType.L2);
+            return emd;
+        }
+
+        //private static double earthMoverDistance(Image<Gray, float> hist1, Image<Gray, float> hist2, int histWidth, int histHeight)
+        //{
+        //    Matrix<float> signature1 = new Matrix<float>(histWidth * histHeight, 3, 1);
+        //    Matrix<float> signature2 = new Matrix<float>(histWidth * histHeight, 3, 1);
+
+        //    for (int r = 0; r < histHeight; r++)
+        //    {
+        //        for (int c = 0; c < histWidth; c++)
+        //        {
+        //            //matHist1;
+        //            signature1.Data[r * histWidth + c, 0] = hist1.Data[r, c, 0] / 255;
+        //            signature1.Data[r * histWidth + c, 1] = r;
+        //            signature1.Data[r * histWidth + c, 2] = c;
+
+        //            //matHist2;
+        //            signature2.Data[r * histWidth + c, 0] = hist2.Data[r, c, 0] / 255;
+        //            signature2.Data[r * histWidth + c, 1] = r;
+        //            signature2.Data[r * histWidth + c, 2] = c;
+        //        }
+        //    }
+
+        //    double emd = CvInvoke.EMD(signature1, signature2, DistType.L2);
+        //    return emd;
+        //}
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            lblCorrel.Text = "Correlation: Computing...";
+            lblChisqr.Text = "Chi-Square: Computing...";
+            lblIntersect.Text = "Intersection: Computing...";
+            lblBhattacharyya.Text = "Bhattacharyya: Computing...";
+            lblChisqrAlt.Text = "Alternative Chi-Square: Computing...";
+            lblEMD.Text = "EMD: Computing...";
+
+            double correl = CvInvoke.CompareHist(HistImg1, HistImg2, HistogramCompMethod.Correl);
+            double chisqr = CvInvoke.CompareHist(HistImg1, HistImg2, HistogramCompMethod.Chisqr);
+            double intersection = CvInvoke.CompareHist(HistImg1, HistImg2, HistogramCompMethod.Intersect);
+            double bhattach = CvInvoke.CompareHist(HistImg1, HistImg2, HistogramCompMethod.Bhattacharyya);
+            double chisqralt = CvInvoke.CompareHist(HistImg1, HistImg2, HistogramCompMethod.ChisqrAlt);
+            //double emd = 0;
+            double emd = earthMoverDistance(HistValF1, HistValF2, BinSize);
+
+            lblCorrel.Text = "Correlation: " + correl.ToString();
+            lblChisqr.Text = "Chi-Square: " + chisqr.ToString();
+            lblIntersect.Text = "Intersection: " + intersection.ToString();
+            lblBhattacharyya.Text = "Bhattacharyya: " + bhattach.ToString();
+            lblChisqrAlt.Text = "Alternative Chi-Square: " + chisqralt.ToString();
+            lblEMD.Text = "EMD: " + emd.ToString();
+        }
+    }
+}
