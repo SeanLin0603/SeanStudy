500e0a985c7a66789bfd5f37fc60c57af7147f91
sean <sean870603tw@yahoo.com.tw>
2021-05-22 16:37:51 +0800

    Modify DLL directory

--- SeanStudy/SeanStudy/Form1.cs
+++ SeanStudy/SeanStudy/Form1.cs
@@ -9,9 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Corex.VisionLib.Algorithm;
-using Corex.VisionLib;
-using System.Diagnostics;
+//using Corex.VisionLib.Algorithm;
+//using Corex.VisionLib;
 using Emgu.CV.CvEnum;
 
 namespace SeanStudy
